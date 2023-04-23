b84ad5ccd32e512314705cd3daa3a0c4f6307804
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2023-04-23 17:56:48 +0100

    Add tests for meta protocol

--- test/test_baseJOS.jl
+++ test/test_baseJOS.jl
@@ -1,22 +1,25 @@
-using Main.JuliaObjectSystem
 using Test
 
 @testset "Test Top, Object and Class" begin
-    @test getfield(Top, :slots)[:name] == :Top
-    @test getfield(Top, :slots)[:class_precedence_list] == [Top]
-    @test getfield(Top, :slots)[:direct_superclasses] == []
-    @test getfield(Top, :slots)[:direct_slots] == []
-    @test getfield(Top, :slots)[:slots] == []
+    @test class_name(Top) == :Top
+    @test class_cpl(Top)  == [Top]
+    @test class_direct_superclasses(Top) == []
+    @test class_direct_slots(Top)  == []
+    @test class_slots(Top)  == []
+    @test class_of(Top) === Class
 
-    @test getfield(Object, :slots)[:name] == :Object
-    @test getfield(Object, :slots)[:class_precedence_list] == [Object, Top]
-    @test getfield(Object, :slots)[:direct_superclasses] == [Top]
-    @test getfield(Object, :slots)[:direct_slots] == []
-    @test getfield(Object, :slots)[:slots] == []
 
-    @test getfield(Class, :slots)[:name] == :Class
-    @test getfield(Class, :slots)[:class_precedence_list] == [Class, Object, Top]
-    @test getfield(Class, :slots)[:direct_superclasses] == [Object]
-    @test length(getfield(Class, :slots)[:direct_slots]) == 5
-    @test length(getfield(Class, :slots)[:slots]) == 5
+    @test class_name(Object) == :Object
+    @test class_cpl(Object)  == [Object, Top]
+    @test class_direct_superclasses(Object) == [Top]
+    @test class_direct_slots(Object)  == []
+    @test class_slots(Object)  == []
+    @test class_of(Object) === Class
+
+    @test class_name(Class) == :Class
+    @test class_cpl(Class)  == [Class, Object, Top]
+    @test class_direct_superclasses(Class) == [Object]
+    @test length(class_direct_slots(Class))  == 5
+    @test length(class_slots(Class))  == 5
+    @test class_of(Class) === Class
 end
